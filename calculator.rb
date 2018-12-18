5efe5f795bbea5bf65c60f484ecc8ad8b6bbbc71
omustafa1994 <omustafa1994@gmail.com>
2018-12-18 20:24:10 +0000

    working calculator

--- calculator.rb
+++ calculator.rb
@@ -1,36 +1,36 @@
 # CALCULATOR
 puts "Enter number, operation, number"
 val1 = gets.to_i #first value into int
-sym = gets.chomp #get symbol value 
+op = gets.chomp #gets operation 
 val2 = gets.to_i #second value into int
 
-def addition(val1, val2)
-    return val1+val2
-end
-add = addition(val1, val2)
-
-def subtraction(val1, val2)
-    return val1-val2
-end 
-sub = subtraction(val1, val2)
-
-def division(val1, val2)
-    return val1/val2
-end 
-div = division(val1, val2)
-
-def multiplication(val1, val2)
-    return val1*val2
-end
-mult = multiplication(val1, val2)
-
-if sym == "+"
-puts add
-elsif sym == "-"
+if op == "+" #if addition 
 puts "#{val1} - #{val2} = #{val1 - val2}"
-elsif sym == "/"
-puts div
-elsif sym == "*"
-puts mult
+elsif op == "-" #if subtraction 
+puts "#{val1} - #{val2} = #{val1 - val2}"
+elsif op == "/" #if division 
+  puts "#{val1} / #{val2} = #{val1 / val2}"
+elsif op == "*" #if multiplication 
+  puts "#{val1} * #{val2} = #{val1 * val2}"
 else puts "error"
 end 
+
+# def addition(val1, val2)
+#     return val1+val2
+# end
+# add = addition(val1, val2)
+
+# def subtraction(val1, val2)
+#     return val1-val2
+# end 
+# sub = subtraction(val1, val2)
+
+# def division(val1, val2)
+#     return val1/val2
+# end 
+# div = division(val1, val2)
+
+# def multiplication(val1, val2)
+#     return val1*val2
+# end
+# mult = multiplication(val1, val2)
