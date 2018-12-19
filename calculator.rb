5943c97f77726f637379d56583b9e8f4dac28f25
omustafa1994 <omustafa1994@gmail.com>
2018-12-19 09:41:16 +0000

    with if and functions

--- calculator.rb
+++ calculator.rb
@@ -6,33 +6,44 @@ op = gets.chomp #gets operation
 puts "Enter number"
 val2 = gets.to_i #second value into int
 
-if op == "+" #if addition 
-puts "Answer to #{val1} - #{val2} is #{val1 - val2}"
-elsif op == "-" #if subtraction 
-puts "Answer to #{val1} - #{val2} is #{val1 - val2}"
-elsif op == "/" #if division 
-  puts "Answer to #{val1} / #{val2} is #{val1 / val2}"
-elsif op == "*" #if multiplication 
-  puts "Answer to #{val1} * #{val2} is #{val1 * val2}"
-else puts "error"
+# if op == "+" #if addition 
+# puts "Answer to #{val1} - #{val2} is #{val1 - val2}"
+# elsif op == "-" #if subtraction 
+# puts "Answer to #{val1} - #{val2} is #{val1 - val2}"
+# elsif op == "/" #if division 
+#   puts "Answer to #{val1} / #{val2} is #{val1 / val2}"
+# elsif op == "*" #if multiplication 
+#   puts "Answer to #{val1} * #{val2} is #{val1 * val2}"
+# else puts "error"
+# end 
+
+def addition(val1, val2)
+    return val1+val2
+end
+add = addition(val1, val2)
+
+def subtraction(val1, val2)
+    return val1-val2
 end 
+sub = subtraction(val1, val2)
 
-# def addition(val1, val2)
-#     return val1+val2
-# end
-# add = addition(val1, val2)
+def division(val1, val2)
+    return val1/val2
+end 
+div = division(val1, val2)
 
-# def subtraction(val1, val2)
-#     return val1-val2
-# end 
-# sub = subtraction(val1, val2)
+def multiplication(val1, val2)
+    return val1*val2
+end
+mult = multiplication(val1, val2)
 
-# def division(val1, val2)
-#     return val1/val2
-# end 
-# div = division(val1, val2)
-
-# def multiplication(val1, val2)
-#     return val1*val2
-# end
-# mult = multiplication(val1, val2)
+if op == "+" #if addition 
+  puts add
+  elsif op == "-" #if subtraction 
+  puts sub
+  elsif op == "/" #if division 
+    puts div
+  elsif op == "*" #if multiplication 
+    puts mult
+  else puts "error"
+  end 
